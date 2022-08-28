5e4ae6b943f1df8fb63360c894eb810db0455861
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-28 15:34:48 +0530

    final deployment script

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -3,30 +3,24 @@ require("dotenv").config({ path: ".env" });
 const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
 
 async function main() {
-  // Address of the whitelist contract that you deployed in the previous module
+  // Address of the whitelist contract that we deployed previously
   const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
   // URL from where we can extract the metadata for a Crypto Dev NFT
   const metadataURL = METADATA_URL;
-  /*
-  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so cryptoDevsContract here is a factory for instances of our CryptoDevs contract.
-  */
+  
   const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
 
-  // deploy the contract
   const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
     metadataURL,
     whitelistContract
   );
 
-  // print the address of the deployed contract
   console.log(
     "Crypto Devs Contract Address:",
     deployedCryptoDevsContract.address
   );
 }
 
-// Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
