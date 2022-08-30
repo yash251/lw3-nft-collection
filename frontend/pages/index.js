db0ffc7594b8f3654a61c0b875ae0ba0fd4a0bc3
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-30 19:51:49 +0530

    useEffect

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -195,6 +195,38 @@ export default function Home() {
     }
   };
 
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: 'rinkeby',
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+
+      const _presaleStarted = checkIfPresaleStarted();
+      if (_presaleStarted) {
+        checkIfPresaleEnded();
+      }
+      getTokenIdsMinted();
+
+      // Set an interval which gets called every 5 seconds to check presale has ended
+      const presaleEndedInterval = setInterval(async function () {
+        const _presaleStarted = await checkIfPresaleStarted();
+        if (!_presaleStarted) {
+          const _presaleEnded = await checkIfPresaleEnded();
+          if (_presaleEnded) {
+            clearInterval(presaleEndedInterval);
+          }
+        }
+      }, 5 * 1000);
+
+      setInterval(async function () {
+        await getTokenIdsMinted();
+      }, 5 * 1000);
+    }
+  }, [walletConnected]);
+
   return (
     <div className={styles.container}>
       <Head>
