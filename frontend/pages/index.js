1412e2dee46434c33939314cc69276470376b819
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-31 03:19:32 +0530

    img link

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -297,7 +297,7 @@ export default function Home() {
           {/* {renderButton()} */}
         </div>
         <div>
-          <img className={styles.image} src='./cryptodevs/0.svg' />
+          <img className={styles.image} src='./crypto-devs.svg' />
         </div>
       </div>
 
