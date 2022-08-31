d0e8dbe1f5a76bba6351d6589a2f88ab52d4af22
Yash Jagtap <jagtapyash2512@gmail.com>
2022-08-31 14:41:48 +0530

    added dependency

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -224,7 +224,7 @@ export default function Home() {
         await getTokenIdsMinted();
       }, 5 * 1000);
     }
-  }, []);
+  }, [walletConnected]);
 
   const renderButton = () => {
     if (!walletConnected) {
