7283f940ee4de88b0f097745f98ab25cce154fe1
Yash Jagtap <jagtapyash2512@gmail.com>
2022-09-02 15:03:58 +0530

    new metadata endpoint

--- smart_contract/constants/index.js
+++ smart_contract/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that we deployed previously
 const WHITELIST_CONTRACT_ADDRESS = "0x7615FCD8f37b68bcDa787ECc32B99406C0c3c0fA";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection.vercel.app/api/";
+const METADATA_URL = "https://lw3-nft-collection-gamma.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
